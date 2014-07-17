0965ff0fca679fbff5578ee33bee5bb23dac20f8
Dmytro Makhno <dmakhno@users.noreply.github.com>
2014-07-17 15:39:34 +0300

    Metadata fix [ci skip]

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer 'Cameron Johnston'
-maintainer_email 'cameron@needle.com'
-description 'installs components required for running selected flavors of selenium'
+name 'selenium'
+maintainer 'Dmytro Makhno'
+maintainer_email 'support@qubell.com'
+description 'installs components required for running selected flavors of selenium and selenium grid'
 version '0.0.3'
 %w{ java python runit }.each { |cb| depends cb }
