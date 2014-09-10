912af1ef4307a30124f4a87c30e581b06ed11780
gsilantyev <gsilantyev@griddynamics.com>
2014-09-10 14:49:01 +0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,4 +22,4 @@ default.selenium.xvfb.display = ':98'
 default.selenium.xvfb.fbsize = '1280x1024x16'
 
 default.selenium.chromedriver_version = '2.9'
-default.selenium.firefox_version = '29.0.1'
+default.selenium.firefox_version = ''
