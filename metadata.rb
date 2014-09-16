d1dcc3b4ef7ace3661cc632aa2c7399848d85fde
Dmitry Zamaruev <dzamaruev@qubell.com>
2014-09-16 12:39:37 +0300

    Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ name 'selenium'
 maintainer 'Cameron Johnston, Dmytro Makhno, Vasyl Khomenko, Artem Ivantsov'
 maintainer_email 'support@qubell.com'
 description 'installs components required for running selected flavors of selenium and selenium grid'
-version '0.0.3'
+version '1.0.0'
 %w{ java python runit }.each { |cb| depends cb }
